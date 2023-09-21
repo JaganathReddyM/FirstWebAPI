0f5868e084e52663be56c9af60033c8babaeb0b7
Jaganath Reddy Mandaluri <jaganathreddy.m@gavstech.com>
2023-09-21 12:04:08 +0530

    Fist WebApi Commit

---
+++ FirstWebAPI/Controllers/CalculaterController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculaterController : ControllerBase
+    {
+        // api/calculater/add?x=10&y=20
+        [HttpGet("/addCalculater")]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        [HttpGet("/sum")]
+        public int Sum(int x, int y)
+        {
+            return x + y+1000;
+        }
+        // api/calculater/subtract?x=200&y=100
+        [HttpPost]
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        // api/calculater/multiply?x=10&y=20
+        [HttpPut]
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        // api/calculater/divide?x=10&y=20
+        [HttpDelete]
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ FirstWebAPI/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("Demo/add")]
+        public int Add(int x, int y)
+        {
+            return x + y+5000;
+        }
+    }
+}
