3dbd74b62176e0115a7328322c912cee3bf69442
JaganathReddyM <jaganathreddy.m@gavstech.com>
2023-09-21 14:56:04 +0530

    update

---
+++ FirstWebAPI/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstWebAPI.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int Cost { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
