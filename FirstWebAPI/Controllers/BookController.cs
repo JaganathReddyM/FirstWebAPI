b50e792a972c92aa7ca8bdcf36c9e45ade3b9d3a
Jaganath Reddy <145546061+JaganathReddyM@users.noreply.github.com>
2023-09-21 14:44:09 +0530

    Add files via upload

---
+++ FirstWebAPI/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using FirstWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        static List<Book> bookList = new List<Book>();
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Book book = new Book();
+                book.BookID = i;
+                book.Title = "Atomic Habits" + i;
+                book.Cost = i * 100;
+                book.AuthorName = "Author " + i;
+                bookList.Add(book);
+            }
+            return bookList;
+        }
+        [HttpPost]
+        public int AddBook(Book book)
+        {
+            bookList.Add(book);
+            return 1;
+        }
+    }
+}
