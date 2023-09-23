6c03b0af47207f4f97fbf2eec75dc9ab27ddae03
Jaganath Reddy <145546061+JaganathReddyM@users.noreply.github.com>
2023-09-23 14:05:10 +0530

    Update EmpAPIClient

--- WebAPIClientConsole/EmployeeAPIClient.cs
+++ WebAPIClientConsole/EmployeeAPIClient.cs
@@ -1,7 +1,8 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -32,6 +33,37 @@ namespace WebAPIClientConsole
                     {
                         await Console.Out.WriteLineAsync($"{emp.EmpId},{emp.FirstName},{emp.LastName}");
                     }
+                }
+            }
+        }
+        public static async Task AddnewEmployee()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = uri;
+                client.DefaultRequestHeaders.Accept
+                    .Add(new MediaTypeWithQualityHeaderValue ("application/json"));
+                Employee employee = new Employee()
+                {
+                    FirstName ="William",
+                    LastName = "John",
+                    City = "Nyc",
+                    Birthdate = new DateTime(1980,01,01),
+                    HireDate = new DateTime(2000,01,01),
+                    Title = "Manager"
+                };
+                var myContent = JsonConvert.SerializeObject(employee);
+                var buffer = Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = 
+                    new MediaTypeHeaderValue("application/json");
+                //HttpPost:
+                HttpResponseMessage response =
+                    await client.PostAsync("AddNewEmployees", byteContent);
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    await Console.Out.WriteLineAsync(response.StatusCode.ToString());
                 }
             }
         }
