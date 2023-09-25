8f491277d5ba7285e55c3294ce3d5f13eaaeea36
Jaganath Reddy <145546061+JaganathReddyM@users.noreply.github.com>
2023-09-25 22:14:00 +0530

    Commit Changes

--- WebAPIClientConsole/Program.cs
+++ WebAPIClientConsole/Program.cs
@@ -1,6 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using WebAPIClientConsole;
 
 Console.WriteLine("API Client!");
 EmployeeAPIClient.CallGetAllEmployee().Wait();
 Console.ReadLine();
+EmployeeAPIClient.AddnewEmployee().Wait();
+Console.WriteLine();
+EmployeeAPIClient.DeleteEmployee(10).Wait();
+Console.WriteLine();
+
+EmpViewModel empToUpdate = new EmpViewModel()
+{
+    EmpId = 10,
+    FirstName = "Update",
+    LastName = "Update",
+    City = "Nyc",
+    Birthdate = new DateTime(1980, 01, 01),
+    HireDate = new DateTime(2000, 01, 01),
+    Title = "Manager"
+
+};
+
+EmployeeAPIClient.UpdateEmployee(10, empToUpdate).Wait();
