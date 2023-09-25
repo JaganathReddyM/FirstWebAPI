a32c4e2f2ec4ddae700851e49b758af20261e853
Jaganath Reddy <145546061+JaganathReddyM@users.noreply.github.com>
2023-09-25 22:12:50 +0530

    Update Api

--- WebAPIClientConsole/EmployeeAPIClient.cs
+++ WebAPIClientConsole/EmployeeAPIClient.cs
@@ -17,7 +17,7 @@ namespace WebAPIClientConsole
             using (var client = new HttpClient())
             {
                 client.BaseAddress = uri;
-                List<Employee> employees = new List<Employee>();
+                List<EmpViewModel> employees = new List<EmpViewModel>();
                 client.DefaultRequestHeaders
                     .Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
@@ -28,8 +28,8 @@ namespace WebAPIClientConsole
                 {
                     String json = await response.Content.ReadAsStringAsync();
                     //install Newtonsoft.Json using package installer
-                    employees = JsonConvert.DeserializeObject<List<Employee>>(json);
-                    foreach(Employee emp in employees)
+                    employees = JsonConvert.DeserializeObject<List<EmpViewModel>>(json);
+                    foreach(EmpViewModel emp in employees)
                     {
                         await Console.Out.WriteLineAsync($"{emp.EmpId},{emp.FirstName},{emp.LastName}");
                     }
@@ -43,7 +43,7 @@ namespace WebAPIClientConsole
                 client.BaseAddress = uri;
                 client.DefaultRequestHeaders.Accept
                     .Add(new MediaTypeWithQualityHeaderValue ("application/json"));
-                Employee employee = new Employee()
+                EmpViewModel employee = new EmpViewModel()
                 {
                     FirstName ="William",
                     LastName = "John",
@@ -64,6 +64,85 @@ namespace WebAPIClientConsole
                 if (response.IsSuccessStatusCode)
                 {
                     await Console.Out.WriteLineAsync(response.StatusCode.ToString());
+                }
+            }
+        }
+        public static async Task UpdateEmployee(int id, EmpViewModel employee)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = uri;
+                client.DefaultRequestHeaders.Accept
+                    .Add(new MediaTypeWithQualityHeaderValue ("application/json"));
+                var myContent = JsonConvert.SerializeObject(employee);   //Serialzing object to JSON
+                var buffer = Encoding.UTF8.GetBytes(myContent);          // Convert JSON string to byte array
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType =
+                    new MediaTypeHeaderValue("application/json");
+                //HttpPost:
+                HttpResponseMessage response =
+                    await client.PutAsync($"UpdateEmployee/{id}", byteContent);
+                response.EnsureSuccessStatusCode();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Employee updated Successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to update the employee. Status Code:{response.StatusCode}.Reason:{response.Content}");
+                }
+            }
+        }
+        public static async Task EditEmployee(int id, EmpViewModel employee)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = uri;
+                client.DefaultRequestHeaders.Accept
+                    .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var myContent = JsonConvert.SerializeObject(employee);   //Serialzing object to JSON
+                var buffer = Encoding.UTF8.GetBytes(myContent);          // Convert JSON string to byte array
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType =
+                    new MediaTypeHeaderValue("application/json");
+                //HttpPost:
+                HttpResponseMessage response =
+                    await client.PutAsync($"EditEmployee/{id}", byteContent);
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Employee edited Successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"Error editing the employee. Status Code:{response.StatusCode}.Reason:{response.Content}");
+                }
+
+            }
+        }
+        public static async Task DeleteEmployee(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = uri;
+                client.DefaultRequestHeaders.Accept
+                    .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                EmpViewModel emp = new EmpViewModel();
+                emp.EmpId = id;
+                //HttpPost:
+                HttpResponseMessage response =
+                    await client.DeleteAsync($"DeleteEmployee/{id}");
+                response.EnsureSuccessStatusCode();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Employee deleted Successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"Error deleting the employee. Status Code:{response.StatusCode}.Reason:{response.Content}");
                 }
             }
         }
